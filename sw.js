040909d19d90ee6e8ad09dc4dc8a1dbf994ab99f
Ahmedcow <137366021+Ahmedcow@users.noreply.github.com>
2026-09-23 09:47:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
-const CACHE='medex-v5-20260923';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
+const CACHE = 'medex-v6';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET')return;
-  const url=new URL(req.url);
-  if(url.pathname.includes('/api/'))return;
-  event.respondWith(
-    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
-      if(res.ok && (url.origin===location.origin || url.pathname.includes('/questions/'))){
-        const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy));
-      }
-      return res;
-    }).catch(()=>caches.match('./index.html').then(x=>x||caches.match('./'))))
-  );
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.includes('/api/')) return;
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
 });
